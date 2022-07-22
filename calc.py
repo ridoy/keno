a5142e76efe6d581bc698c353ad1d5ac322fe435
ridoy <majumdar.ridoy@gmail.com>
2022-07-21 20:43:16 -0400

    exp 2

--- calc.py
+++ calc.py
@@ -5,12 +5,18 @@ Various experiments on drawings data
 import json
 
 print('Loading 6 month drawings history...')
-f = open('drawings.json', 'r')
+f = open('data/drawings.json', 'r')
 data = json.loads(f.read())
 
 print('Loaded.')
 
 num_games = len(data)
+counts = {}
+for i in range(1,81):
+    counts[i] = 0
+for game,draws in data.items():
+    for draw in draws:
+        counts[int(draw)] += 1
 
 def print_all(data):
     i = 0
@@ -22,23 +28,6 @@ def print_all(data):
 # experiment 1: % chance of 1-80 in next draw given n was in a previous draw
 # use n=1 to start
 # if n=1 present, check next game, increment odds
-
-
-'''
-for key,value in data.items():
-    for num in value:
-        pct_next_draw[int(num)] += 1
-'''
-
-
-counts = {}
-for i in range(1,81):
-    counts[i] = 0
-for game,draws in data.items():
-    for draw in draws:
-        counts[int(draw)] += 1
-print(counts)
-
 
 
 
@@ -73,21 +62,42 @@ for k,v in count_next_draw.items():
 
 # find max and min + pct for each for all keys of count_nexT_draw
 
-print('Loading ohter file...')
-f = open('exp1.json', 'r')
-count_next_draw = json.loads(f.read())
+def subsequent_draw_freqs():
+    print('Loading ohter file...')
+    f = open('exp1.json', 'r')
+    count_next_draw = json.loads(f.read())
 
 # for k,v in count_next_draw.items(): # k= 1 through 80, v=drawcounts next game
-for i in range(1,81):
-    # find max
-    print('if ' + str(i) + ' appears in one game, the most and least likely numbers in the next game are:')
-    print(max(count_next_draw[str(i)], key=count_next_draw[str(i)].get))
-    print(float(max(count_next_draw[str(i)].values())) / counts[int(i)])
-    print(min(count_next_draw[str(i)], key=count_next_draw[str(i)].get))
-    print(float(min(count_next_draw[str(i)].values())) / counts[int(i)])
+    for i in range(1,81):
+        # find max
+        print('if ' + str(i) + ' appears in one game, the most and least likely numbers in the next game are:')
+        print(max(count_next_draw[str(i)], key=count_next_draw[str(i)].get))
+        print(float(max(count_next_draw[str(i)].values())) / counts[int(i)])
+        print(min(count_next_draw[str(i)], key=count_next_draw[str(i)].get))
+        print(float(min(count_next_draw[str(i)].values())) / counts[int(i)])
 
 
 
 #print(count_next_draw)
 
 
+
+# experiment 2: repeat results
+# find all games that have happened >1 times
+
+drawings_to_game = {}
+for k,v in data.items():
+    v.sort(key=int)
+    drawings_to_game[str(v)] = k
+
+print(len(drawings_to_game.values()))
+
+# construct search tree, or hash map probably not so difficult
+
+'''
+for k,v in data.items():
+    print("observing game " + str(k))
+    if (drawings_to_game[str(v)] != k):
+        print(k)
+        print(drawings_to_game[str(v)])
+        '''
