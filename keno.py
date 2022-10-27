e0c5a2810047866127e600c74443e0e3ff48a115
ridoy <majumdar.ridoy@gmail.com>
2022-10-27 09:39:04 -0400

    Refactor keno script

---
+++ keno.py
@@ -0,0 +1,95 @@
+'''
+Various experiments on drawings data
+'''
+
+
+# TODO run 4-pick over all data (all possible 4 picks = 80 c 4)
+
+import json
+
+class KenoAnalysis:
+
+    # Drawings data should be in format {"gamenum": [num,num,...num]}
+    # Drawings history between games 2098753 and 2163865
+    drawings_data_file = 'data/drawings.json'
+
+    def load_drawings(self):
+        print('Loading 6 month drawings history...')
+        f = open(self.drawings_data_file, 'r')
+        self.data = json.loads(f.read())
+        self.num_games = len(self.data)
+        self.first_game_index = min(self.data.keys())
+        self.last_game_index = max(self.data.keys())
+        print('Loaded.')
+
+    def print_all(self, data):
+        i = 0
+        for key,value in self.data.items():
+            print(key,value);
+            if (i > 20):
+                break
+
+    # experiment 1: % chance of 1-80 in next draw given n was in a previous draw
+    # use n=1 to start
+    # if n=1 present, check next game, increment odds
+    def experiment_one(self):
+
+        '''
+        for key,value in data.items():
+            for num in value:
+                pct_next_draw[int(num)] += 1
+        '''
+
+        counts = [0 for x in range(1,81)]
+        for game,draws in self.data.items():
+            for draw in draws:
+                counts[int(draw)] += 1
+        print(counts)
+
+    def old_stuff():
+        first_game_index = 2098753
+        last_game_index = 2163853
+        count_next_draw = {}
+        pct_next_draw = {}
+
+        for i in range(1,81):
+            count_next_draw[i] = 0
+        # creates mapping from number and percent each number will be drawn the next game
+        for n in range(1,81):
+            count_next_draw[n] = {}
+            for i in range(1,81):
+                count_next_draw[n][i] = 0
+            for i in range(first_game_index, last_game_index):
+                # if n=1 present, check next game, increment odds
+                if (str(n) in data[str(i)]):
+                    for num in data[str(i+1)]:
+                        count_next_draw[n][int(num)] += 1
+
+        with open('exp1.json', 'w') as f:
+            json.dump(count_next_draw, f)
+
+        '''
+        for k,v in count_next_draw.items():
+            n_draw_count =
+            pct_next_draw[k] = round(float(v) / 16223, 5)
+        '''
+
+        # find max and min + pct for each for all keys of count_nexT_draw
+
+        print('Loading ohter file...')
+        f = open('exp1.json', 'r')
+        count_next_draw = json.loads(f.read())
+
+        # for k,v in count_next_draw.items(): # k= 1 through 80, v=drawcounts next game
+        for i in range(1,81):
+            # find max
+            print('if ' + str(i) + ' appears in one game, the most and least likely numbers in the next game are:')
+            print(max(count_next_draw[str(i)], key=count_next_draw[str(i)].get))
+            print(float(max(count_next_draw[str(i)].values())) / counts[int(i)])
+            print(min(count_next_draw[str(i)], key=count_next_draw[str(i)].get))
+            print(float(min(count_next_draw[str(i)].values())) / counts[int(i)])
+
+
+
+keno = KenoAnalysis()
+keno.load_drawings()
